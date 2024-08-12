36ee7ca25abb5005bb6d13c7acb584d77255de55
Ilia <51325702+treger78@users.noreply.github.com>
2024-08-12 15:05:21 +0300

    Update index.js

--- map.erarzn.ru/js/index.js
+++ map.erarzn.ru/js/index.js
@@ -1,4 +1,5 @@
 import trashPoints from './trash_points.js';
+import { CONSTS } from './constants.js';
 
 document.getElementById("current-year").textContent = new Date().getFullYear();
 
@@ -6,59 +7,47 @@ const newMapPointsTable = document.getElementById("new-map-points-table-body");
 
 for (let i = trashPoints.length - 1; i > trashPoints.length - 11; i -= 1) {
     const trashPoint = trashPoints[i];
-    let trashCategory;
-    let trashStatus;
-    let trashStatusColor;
-    
-    switch (trashPoint.categ) {
-        case '10': trashCategory = 'строительный мусор';
-        break;
-
-        case '20': trashCategory = 'бытовые отходы';
-        break;
-
-        case '30': trashCategory = 'промышленные отходы';
-        break;
-    }
-
-    switch (trashPoint.status) {
-        case "-20": {
-            trashStatus = 'отменена';
-            trashStatusColor = 'gray';
-        }; break;
-
-        case "0": {
-            trashStatus = 'не проверена';
-            trashStatusColor = 'yellow';
-        }; break;
-
-        case "10": {
-            trashStatus = 'переданно в спец. органы';
-            trashStatusColor = 'orange';
-        }; break;
-
-        case "20": {
-            trashStatus = 'проверена';
-            trashStatusColor = 'red';
-        }; break;
-        
-        case "30": {
-            trashStatus = 'идет уборка';
-            trashStatusColor = 'lightBlue';
-        }; break;
-
-        case "40": {
-            trashStatus = 'убрана';
-            trashStatusColor = 'green';
-        }; break;
-    }
 
     newMapPointsTable.insertAdjacentHTML('beforeend', `
     <td><small>Дата</small></td>
     <td>${trashPoint.id}</td>
     <td class="trash-point-name">${trashPoint.name}</td>
-    <td><span style="background-color: ${trashStatusColor}">${trashStatus}</span></td>
-    <td>${trashCategory}</td>
+    <td><span style="background-color: ${CONSTS.trashStatus[trashPoint.status].color}">${CONSTS.trashStatus[trashPoint.status].name}</span></td>
+    <td>${CONSTS.trashCategory[trashPoint.categ].name}</td>
     <td class="volunteer-name">Волонтер</td>
     `);
 }
+
+//trashCategory - вид отмеченного на карте мусора:
+//10 - строительный мусор
+//20 - бытовые отходы
+//30 - промышленные
+
+//trashStatus - статус отмеченного на карте мусора:
+//-20 - gray, отменена
+//0 - yellow, не проверена
+//10 - orange, переданно в спец. органы
+//20 - red, проверена
+//30 - #33ffff ~ligthblue, идет уборка
+//40 - green, убрана
+
+const trashCategory = document.getElementById("trash-category");
+const trashStatus = document.getElementById("trash-status");
+
+trashCategory.insertAdjacentHTML('beforeend', `
+    <b>Виды мусора на карте:</b>
+    <div>${CONSTS.trashCategory[10].name}</div>
+    <div>${CONSTS.trashCategory[20].name}</div>
+    <div>${CONSTS.trashCategory[30].name}</div>
+`);
+
+trashStatus.insertAdjacentHTML('beforeend', `
+    <b>Статус:</b>
+    <div>Все статусы</div>
+    <div style="background-color: ${CONSTS.trashStatus['-20'].color}">${CONSTS.trashStatus['-20'].name}</div>
+    <div style="background-color: ${CONSTS.trashStatus['0'].color}">${CONSTS.trashStatus['0'].name}</div>
+    <div style="background-color: ${CONSTS.trashStatus['10'].color}">${CONSTS.trashStatus['10'].name}</div>
+    <div style="background-color: ${CONSTS.trashStatus['20'].color}">${CONSTS.trashStatus['20'].name}</div>
+    <div style="background-color: ${CONSTS.trashStatus['30'].color}">${CONSTS.trashStatus['30'].name}</div>
+    <div style="background-color: ${CONSTS.trashStatus['40'].color}">${CONSTS.trashStatus['40'].name}</div>
+`);
