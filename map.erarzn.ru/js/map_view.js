e0eaea1e3ebc071103e61ac27ce544b4bc9100f4
Ilia <51325702+treger78@users.noreply.github.com>
2024-08-12 15:05:41 +0300

    Update map_view.js

--- map.erarzn.ru/js/map_view.js
+++ map.erarzn.ru/js/map_view.js
@@ -1,3 +1,4 @@
+import { CONSTS } from './constants.js';
 import trashPoints from './trash_points.js';
 
 ymaps.ready(init);
@@ -8,93 +9,27 @@ function init() {
         zoom: 9,
         type: 'yandex#hybrid'
     });
-
-    //categ - вид отмеченного на карте мусора:
-    //10 - строительный мусор
-    //20 - бытовые отходы
-    //30 - промышленные
-
-    //status - статус отмеченного на карте мусора:
-    //-20 - gray, отменена
-    //0 - yellow, не проверена
-    //10 - orange, переданно в спец. органы
-    //20 - red, проверена
-    //30 - #33ffff ~ligthblue, идет уборка
-    //40 - green, убрана
     
     for (let i = 0; i < trashPoints.length; i += 1) {
         const point = trashPoints[i];
-        let iconContent;
-        let trashCategory;
-        let trashStatus;
-        let trashStatusColor;
-
-        switch (point.categ) {
-            case '10': {
-                iconContent = 'С';
-                trashCategory = 'строительный мусор';
-            }; break;
-
-            case '20': {
-                iconContent = 'Б';
-                trashCategory = 'бытовые отходы';
-            }; break;
-
-            case '30': {
-                iconContent = 'П';
-                trashCategory = 'промышленные отходы';
-            }; break;
-        }
-
-        switch (point.status) {
-            case "-20": {
-                trashStatus = 'отменена';
-                trashStatusColor = 'gray';
-            }; break;
-
-            case "0": {
-                trashStatus = 'не проверена';
-                trashStatusColor = 'yellow';
-            }; break;
-
-            case "10": {
-                trashStatus = 'переданно в спец. органы';
-                trashStatusColor = 'orange';
-            }; break;
-
-            case "20": {
-                trashStatus = 'проверена';
-                trashStatusColor = 'red';
-            }; break;
-            
-            case "30": {
-                trashStatus = 'идет уборка';
-                trashStatusColor = 'lightBlue';
-            }; break;
-
-            case "40": {
-                trashStatus = 'убрана';
-                trashStatusColor = 'green';
-            }; break;
-        }
 
         map.geoObjects.add(
             new ymaps.Placemark(
                 [point.lat, point.lng],
                 {
-                    iconContent: iconContent,
+                    iconContent: CONSTS.trashCategory[point.categ].iconContent,
                     balloonContentHeader: `${point.id}. ${point.name}`,
                     balloonContentBody: `
-                    <div><small>${trashCategory}</small></div>
+                    <div><small>${CONSTS.trashCategory[point.categ].name}</small></div>
                     <div><small>Добавил точку: волонтёр</small></div>
-                    <div><small>Статус точки: <span style="background-color: ${trashStatusColor}">${trashStatus}</span></small></div>
+                    <div><small>Статус точки: <span style="background-color: ${CONSTS.trashStatus[point.status].color}">${CONSTS.trashStatus[point.status].name}</span></small></div>
                     `,
                     balloonContentFooter: `
                     <small class="balloon-footer-detail">Подробно →</small>
                     `,
                 },
                 {
-                    preset: `islands#${trashStatusColor}Icon`,
+                    preset: `islands#${CONSTS.trashStatus[point.status].color}Icon`,
                     hideIconOnBalloonOpen: false,
                 }
             )
