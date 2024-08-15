fd5736e6fd30a8747bdf0bd8c08ebb54862858f1
Ilia <51325702+treger78@users.noreply.github.com>
2024-08-15 15:43:43 +0300

    Update map_view.js

--- map.erarzn.ru/js/map_view.js
+++ map.erarzn.ru/js/map_view.js
@@ -57,4 +57,57 @@ function init() {
             }
         }
     });
+
+    //TODO: сделать рефакториг, не было времени причесать код
+    document.getElementById("refresh-map-button").addEventListener('click', () => {
+        const trashCategories = Object.keys(CONSTS.trashCategory);
+        const trashStatuses = Object.keys(CONSTS.trashStatus);
+
+        map.geoObjects.removeAll();
+
+        const selectedTrashCategory = [];
+
+        for (let i = 0; i < trashCategories.length; i += 1) {
+            const trashCategoryCheckbox = document.getElementById(`trashCategory_${trashCategories[i]}`);
+
+            if (trashCategoryCheckbox.checked) selectedTrashCategory.push(trashCategories[i]);
+        }
+
+        const selectedTrashStatuses = [];
+
+        for (let i = 0; i < trashStatuses.length; i += 1) {
+            const trashStatuseCheckbox = document.getElementById(`trashStatus_${trashStatuses[i]}`);
+
+            if (trashStatuseCheckbox.checked) selectedTrashStatuses.push(trashStatuses[i]);
+        }
+
+        for (let i = 0; i < trashPoints.length; i += 1) {
+            const point = trashPoints[i];
+
+            if (selectedTrashCategory.includes(point.categ) && selectedTrashStatuses.includes(point.status)) {
+                map.geoObjects.add(
+                    new ymaps.Placemark(
+                        [point.lat, point.lng],
+                        {
+                            iconContent: CONSTS.trashCategory[point.categ].iconContent,
+                            balloonContentHeader: `${point.id}. ${point.name}`,
+                            balloonContentBody: `
+                            <div><small>${CONSTS.trashCategory[point.categ].name}</small></div>
+                            <div><small>Добавил точку: волонтёр</small></div>
+                            <div><small>Статус точки: <span style="background-color: ${CONSTS.trashStatus[point.status].colorHEX}">${CONSTS.trashStatus[point.status].name}</span></small></div>
+                            `,
+                            balloonContentFooter: `
+                            <small class="balloon-footer-detail">Подробно →</small>
+                            `,
+                        },
+                        {
+                            preset: `islands#${CONSTS.trashStatus[point.status].colorName}Icon`,
+                            hideIconOnBalloonOpen: false,
+                        }
+                    )
+                );
+            }
+        }
+    });
+
 }
