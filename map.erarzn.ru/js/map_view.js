1ea524a8df404a4b62f7548d0e664130f956f989
Ilia <51325702+treger78@users.noreply.github.com>
2024-08-16 12:12:57 +0300

    Update map_view.js

--- map.erarzn.ru/js/map_view.js
+++ map.erarzn.ru/js/map_view.js
@@ -9,11 +9,9 @@ function init() {
         zoom: 9,
         type: 'yandex#hybrid'
     });
-    
-    for (let i = 0; i < trashPoints.length; i += 1) {
-        const point = trashPoints[i];
-
-        map.geoObjects.add(
+  
+    const addPlacemark = (point, CONSTS) => {
+        return map.geoObjects.add(
             new ymaps.Placemark(
                 [point.lat, point.lng],
                 {
@@ -34,6 +32,10 @@ function init() {
                 }
             )
         );
+    };
+
+    for (let i = 0; i < trashPoints.length; i += 1) {
+        addPlacemark(trashPoints[i], CONSTS);
     }
 
     document.getElementById("total-trash-points").textContent = map.geoObjects.getLength();
@@ -58,56 +60,30 @@ function init() {
         }
     });
 
-    //TODO: сделать рефакториг, не было времени причесать код
-    document.getElementById("refresh-map-button").addEventListener('click', () => {
-        const trashCategories = Object.keys(CONSTS.trashCategory);
-        const trashStatuses = Object.keys(CONSTS.trashStatus);
+    const getSelectedManageMapViewPointsCheckboxes = (trashMapViewFiltersIndexes, trashMapViewManageName) => {
+        const selectedTrashMapViewManageCheckboxes = [];
 
+        for (let i = 0; i < trashMapViewFiltersIndexes.length; i += 1) {
+            const trashMapViewManageCheckbox = document.getElementById(`${trashMapViewManageName}_${trashMapViewFiltersIndexes[i]}`);
+
+            if (trashMapViewManageCheckbox.checked) selectedTrashMapViewManageCheckboxes.push(trashMapViewFiltersIndexes[i]);
+        }
+
+        return selectedTrashMapViewManageCheckboxes;
+    };
+
+    document.getElementById("refresh-map-button").addEventListener('click', () => {
         map.geoObjects.removeAll();
 
-        const selectedTrashCategory = [];
-
-        for (let i = 0; i < trashCategories.length; i += 1) {
-            const trashCategoryCheckbox = document.getElementById(`trashCategory_${trashCategories[i]}`);
-
-            if (trashCategoryCheckbox.checked) selectedTrashCategory.push(trashCategories[i]);
-        }
-
-        const selectedTrashStatuses = [];
-
-        for (let i = 0; i < trashStatuses.length; i += 1) {
-            const trashStatuseCheckbox = document.getElementById(`trashStatus_${trashStatuses[i]}`);
-
-            if (trashStatuseCheckbox.checked) selectedTrashStatuses.push(trashStatuses[i]);
-        }
+        const selectedTrashCategory = getSelectedManageMapViewPointsCheckboxes(Object.keys(CONSTS.trashCategory), 'trashCategory');
+        const selectedTrashStatuses = getSelectedManageMapViewPointsCheckboxes(Object.keys(CONSTS.trashStatus), 'trashStatus');;
 
         for (let i = 0; i < trashPoints.length; i += 1) {
             const point = trashPoints[i];
 
             if (selectedTrashCategory.includes(point.categ) && selectedTrashStatuses.includes(point.status)) {
-                map.geoObjects.add(
-                    new ymaps.Placemark(
-                        [point.lat, point.lng],
-                        {
-                            iconContent: CONSTS.trashCategory[point.categ].iconContent,
-                            balloonContentHeader: `${point.id}. ${point.name}`,
-                            balloonContentBody: `
-                            <div><small>${CONSTS.trashCategory[point.categ].name}</small></div>
-                            <div><small>Добавил точку: волонтёр</small></div>
-                            <div><small>Статус точки: <span style="background-color: ${CONSTS.trashStatus[point.status].colorHEX}">${CONSTS.trashStatus[point.status].name}</span></small></div>
-                            `,
-                            balloonContentFooter: `
-                            <small class="balloon-footer-detail">Подробно →</small>
-                            `,
-                        },
-                        {
-                            preset: `islands#${CONSTS.trashStatus[point.status].colorName}Icon`,
-                            hideIconOnBalloonOpen: false,
-                        }
-                    )
-                );
+                addPlacemark(point, CONSTS);
             }
         }
     });
-
 }
