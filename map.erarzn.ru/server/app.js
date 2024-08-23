121a975494dc950d0ab03ad49661b599ae1da49c
Ilia <ilya.pustograev.i@mail.ru>
2024-08-23 14:54:23 +0300

    updated

--- map.erarzn.ru/server/app.js
+++ map.erarzn.ru/server/app.js
@@ -5,15 +5,23 @@ dotenv.config();
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
-    user: process.env.DB_USER,
     database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD
 });
 
+connection.query("SELECT * FROM user", (error, res) => {
+    if (error) console.error(error);
+
+    console.log(res);
+});
+
+/*
 connection.connect((error) => {
     if (error) return console.error("Error: " + error.message);
 
     console.log("Connected");
+    console.log()
 });
 
 connection.end((error) => {
@@ -21,6 +29,7 @@ connection.end((error) => {
 
     console.log("Closed");
 });
+*/
 
 /*
 const http = require("http");
